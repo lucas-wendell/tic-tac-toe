5638b9487f7a1c9f5c6e3a9c5451461b5a8bf1d7
Lucas Wendell <lucaswhendellf@gmail.com>
2022-12-11 15:52:25 -0300

    add game page

---
+++ dist/index.js
@@ -0,0 +1,27 @@
+"use strict";
+const main = document.querySelector(".main");
+const buttons = Array.from(document.querySelectorAll(".chooseButtons button"));
+const filterButton = () => buttons.filter((button) => button.getAttribute("data-js"));
+const actions = {
+    chooseSymbolButton(target) {
+        const dataJs = target.getAttribute("data-js");
+        const [actualButton] = filterButton();
+        if (!(dataJs === "active")) {
+            actualButton.removeAttribute("data-js");
+            target.setAttribute("data-js", "active");
+        }
+    },
+    choosePlayerButton(target) {
+        const [actualButton] = filterButton();
+        const firstPlayer = actualButton.getAttribute("value");
+        const secondPlayerIs = target.getAttribute("value");
+        localStorage.setItem("firstPlayer", firstPlayer);
+        localStorage.setItem("secondPlayerIs", secondPlayerIs);
+    },
+};
+main.addEventListener("click", (e) => {
+    const target = e.target;
+    const funcName = target.getAttribute("data-fn");
+    const fun = actions[funcName];
+    fun === null || fun === void 0 ? void 0 : fun(target);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
-console.log("ola mundo");
+const main = document.querySelector(".main") as HTMLDivElement;
+
+const buttons = Array.from(
+	document.querySelectorAll(".chooseButtons button"),
+) as HTMLButtonElement[];
+
+const filterButton = () =>
+	buttons.filter((button) => button.getAttribute("data-js"));
+
+const actions = {
+	chooseSymbolButton(target: Element) {
+		const dataJs = target.getAttribute("data-js");
+		const [actualButton] = filterButton();
+
+		if (!(dataJs === "active")) {
+			actualButton.removeAttribute("data-js");
+			target.setAttribute("data-js", "active");
+		}
+	},
+
+	choosePlayerButton(target: Element) {
+		const [actualButton] = filterButton();
+		const firstPlayer = actualButton.getAttribute("value") as string;
+		const secondPlayerIs = target.getAttribute("value") as string;
+
+		localStorage.setItem("firstPlayer", firstPlayer);
+		localStorage.setItem("secondPlayerIs", secondPlayerIs);
+	},
+};
+
+main.addEventListener("click", (e: Event) => {
+	type TypeActions = typeof actions;
+	type AllowedKeys = keyof TypeActions;
+
+	const target = e.target as Element;
+	const funcName = target.getAttribute("data-fn");
+
+	const fun = actions[funcName as AllowedKeys];
+	fun?.(target);
+});
